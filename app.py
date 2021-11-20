f2def9222c4351d86f8917752cc2d0c12b689278
Morugar <morugar.shestak@gmail.com>
2021-11-20 19:41:44 +0400

    Upd Dockerfile & app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    url = "https://waggingmongrel.com/wp-content/uploads/2018/10/shutterstock_265071971.jpg"
-    return render_template("index.html", url=url)
+    return render_template("index.html", url="https://waggingmongrel.com/wp-content/uploads/2018/10/shutterstock_265071971.jpg")
 
 
 if __name__ == "__main__":
